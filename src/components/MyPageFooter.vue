ffb10b6ed042f8dd5c4d118a52951b65fa750b2c
hung vu <hungvugithub@gmail.com>
2025-07-29 17:35:35 +0700

    MyPage Footer

---
+++ src/components/MyPageFooter.vue
@@ -0,0 +1,422 @@
+<template>
+  <footer class="mypage-footer">
+    <div class="mypage-footer__container">
+      <div class="mypage-footer__divider"></div>
+      <div class="mypage-footer__content-wrapper">
+        <div class="mypage-footer__info-section">
+          <div class="mypage-footer__info-group">
+            <h3 class="mypage-footer__heading">CÔNG TY TNHH CÔNG NGHỆ MYPAGE</h3>
+            <ul class="mypage-footer__list">
+              <li class="mypage-footer__list-item">
+                <div class="mypage-footer__icon-container">
+                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0ZM8 2C8.55228 2 9 2.44772 9 3V7H13C13.5523 7 14 7.44772 14 8C14 8.55228 13.5523 9 13 9H9V13C9 13.5523 8.55228 14 8 14C7.44772 14 7 13.5523 7 13V9H3C2.44772 9 2 8.55228 2 8C2 7.44772 2.44772 7 3 7H7V3C7 2.44772 7.44772 2 8 2Z" fill="currentColor"/>
+                  </svg>
+                </div>
+                <p class="mypage-footer__text">27 Hoàng Hoa Thám, Phường Gia Định, TP. Hồ Chí Minh</p>
+              </li>
+              <li class="mypage-footer__list-item">
+                <div class="mypage-footer__icon-container">
+                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.5 11.5C15.5 11.7761 15.2761 12 15 12H13.5C13.2239 12 13 11.7761 13 11.5V10.5C13 10.2239 13.2239 10 13.5 10H15C15.2761 10 15.5 10.2239 15.5 10.5V11.5ZM15.5 1.5C15.5 1.77614 15.2761 2 15 2H13.5C13.2239 2 13 1.77614 13 1.5V0.5C13 0.223858 13.2239 0 13.5 0H15C15.2761 0 15.5 0.223858 15.5 0.5V1.5ZM15.5 6.5C15.5 6.77614 15.2761 7 15 7H13.5C13.2239 7 13 6.77614 13 6.5V5.5C13 5.22386 13.2239 5 13.5 5H15C15.2761 5 15.5 5.22386 15.5 5.5V6.5ZM10.5 11.5C10.5 11.7761 10.2761 12 10 12H8.5C8.22386 12 8 11.7761 8 11.5V10.5C8 10.2239 8.22386 10 8.5 10H10C10.2761 10 10.5 10.2239 10.5 10.5V11.5ZM10.5 1.5C10.5 1.77614 10.2761 2 10 2H8.5C8.22386 2 8 1.77614 8 1.5V0.5C8 0.223858 8.22386 0 8.5 0H10C10.2761 0 10.5 0.223858 10.5 0.5V1.5ZM10.5 6.5C10.5 6.77614 10.2761 7 10 7H8.5C8.22386 7 8 6.77614 8 6.5V5.5C8 5.22386 8.22386 5 8.5 5H10C10.2761 5 10.5 5.22386 10.5 5.5V6.5ZM5.5 11.5C5.5 11.7761 5.27614 12 5 12H3.5C3.22386 12 3 11.7761 3 11.5V10.5C3 10.2239 3.22386 10 3.5 10H5C5.27614 10 5.5 10.2239 5.5 10.5V11.5ZM5.5 1.5C5.5 1.77614 5.27614 2 5 2H3.5C3.22386 2 3 1.77614 3 1.5V0.5C3 0.223858 3.22386 0 3.5 0H5C5.27614 0 5.5 0.223858 5.5 0.5V1.5ZM5.5 6.5C5.5 6.77614 5.27614 7 5 7H3.5C3.22386 7 3 6.77614 3 6.5V5.5C3 5.22386 3.22386 5 3.5 5H5C5.27614 5 5.5 5.22386 5.5 5.5V6.5ZM0.5 11.5C0.5 11.7761 0.276142 12 0 12H-1.5C-1.77614 12 -2 11.7761 -2 11.5V10.5C-2 10.2239 -1.77614 10 -1.5 10H0C0.276142 10 0.5 10.2239 0.5 10.5V11.5ZM0.5 1.5C0.5 1.77614 0.276142 2 0 2H-1.5C-1.77614 2 -2 1.77614 -2 1.5V0.5C-2 0.223858 -1.77614 0 -1.5 0H0C0.276142 0 0.5 0.223858 0.5 0.5V1.5ZM0.5 6.5C0.5 6.77614 0.276142 7 0 7H-1.5C-1.77614 7 -2 6.77614 -2 6.5V5.5C-2 5.22386 -1.77614 5 -1.5 5H0C0.276142 5 0.5 5.22386 0.5 5.5V6.5Z" transform="translate(2 2)" fill="currentColor"/>
+                  </svg>
+                </div>
+                <p class="mypage-footer__text">028.66.828.883</p>
+                <span class="mypage-footer__separator">–</span>
+                <div class="mypage-footer__icon-container">
+                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.5 11.5C15.5 11.7761 15.2761 12 15 12H13.5C13.2239 12 13 11.7761 13 11.5V10.5C13 10.2239 13.2239 10 13.5 10H15C15.2761 10 15.5 10.2239 15.5 10.5V11.5ZM15.5 1.5C15.5 1.77614 15.2761 2 15 2H13.5C13.2239 2 13 1.77614 13 1.5V0.5C13 0.223858 13.2239 0 13.5 0H15C15.2761 0 15.5 0.223858 15.5 0.5V1.5ZM15.5 6.5C15.5 6.77614 15.2761 7 15 7H13.5C13.2239 7 13 6.77614 13 6.5V5.5C13 5.22386 13.2239 5 13.5 5H15C15.2761 5 15.5 5.22386 15.5 5.5V6.5ZM10.5 11.5C10.5 11.7761 10.2761 12 10 12H8.5C8.22386 12 8 11.7761 8 11.5V10.5C8 10.2239 8.22386 10 8.5 10H10C10.2761 10 10.5 10.2239 10.5 10.5V11.5ZM10.5 1.5C10.5 1.77614 10.2761 2 10 2H8.5C8.22386 2 8 1.77614 8 1.5V0.5C8 0.223858 8.22386 0 8.5 0H10C10.2761 0 10.5 0.223858 10.5 0.5V1.5ZM10.5 6.5C10.5 6.77614 10.2761 7 10 7H8.5C8.22386 7 8 6.77614 8 6.5V5.5C8 5.22386 8.22386 5 8.5 5H10C10.2761 5 10.5 5.22386 10.5 5.5V6.5ZM5.5 11.5C5.5 11.7761 5.27614 12 5 12H3.5C3.22386 12 3 11.7761 3 11.5V10.5C3 10.2239 3.22386 10 3.5 10H5C5.27614 10 5.5 10.2239 5.5 10.5V11.5ZM5.5 1.5C5.5 1.77614 5.27614 2 5 2H3.5C3.22386 2 3 1.77614 3 1.5V0.5C3 0.223858 3.22386 0 3.5 0H5C5.27614 0 5.5 0.223858 5.5 0.5V1.5ZM5.5 6.5C5.5 6.77614 5.27614 7 5 7H3.5C3.22386 7 3 6.77614 3 6.5V5.5C3 5.22386 3.22386 5 3.5 5H5C5.27614 5 5.5 5.22386 5.5 5.5V6.5ZM0.5 11.5C0.5 11.7761 0.276142 12 0 12H-1.5C-1.77614 12 -2 11.7761 -2 11.5V10.5C-2 10.2239 -1.77614 10 -1.5 10H0C0.276142 10 0.5 10.2239 0.5 10.5V11.5ZM0.5 1.5C0.5 1.77614 0.276142 2 0 2H-1.5C-1.77614 2 -2 1.77614 -2 1.5V0.5C-2 0.223858 -1.77614 0 -1.5 0H0C0.276142 0 0.5 0.223858 0.5 0.5V1.5ZM0.5 6.5C0.5 6.77614 0.276142 7 0 7H-1.5C-1.77614 7 -2 6.77614 -2 6.5V5.5C-2 5.22386 -1.77614 5 -1.5 5H0C0.276142 5 0.5 5.22386 0.5 5.5V6.5Z" transform="translate(2 2)" fill="currentColor"/>
+                  </svg>
+                </div>
+                <p class="mypage-footer__text">0938.771.797</p>
+              </li>
+              <li class="mypage-footer__list-item">
+                <div class="mypage-footer__icon-container">
+                  <svg width="16" height="11" viewBox="0 0 16 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.5 0H0.5C0.223858 0 0 0.223858 0 0.5V10.5C0 10.7761 0.223858 11 0.5 11H15.5C15.7761 11 16 10.7761 16 10.5V0.5C16 0.223858 15.7761 0 15.5 0ZM15.5 1H0.5V10H15.5V1ZM15.5 2L8 6.5L0.5 2V1H15.5V2Z" fill="currentColor"/>
+                  </svg>
+                </div>
+                <p class="mypage-footer__text">info@mypage.vn</p>
+                <span class="mypage-footer__separator">–</span>
+                <p class="mypage-footer__text">support@mypage.vn</p>
+              </li>
+              <li class="mypage-footer__list-item">
+                <p class="mypage-footer__symbol"></p>
+                <p class="mypage-footer__text">Mã số thuế: 0313353606 – Ngày cấp : 17/07/2015</p>
+              </li>
+              <li class="mypage-footer__list-item">
+                <p class="mypage-footer__symbol"></p>
+                <p class="mypage-footer__text">Nơi cấp: Sở Kế Hoạch và Đầu Tư TP. HCM</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="mypage-footer__service-section">
+          <div class="mypage-footer__service-group">
+            <h3 class="mypage-footer__heading">Dịch vụ</h3>
+            <ul class="mypage-footer__list">
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Domain</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Hosting</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Dịch vụ SEO</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Chăm sóc website</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Email doanh nghiệp</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Thiết kế web chuẩn SEO</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="mypage-footer__design-section">
+          <div class="mypage-footer__design-group">
+            <h3 class="mypage-footer__heading">Thiết kế web chuyên nghiệp</h3>
+            <ul class="mypage-footer__list">
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Thiết Kế Web Giá Rẻ</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Thiết Kế Web Theo Mẫu</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Thiết kế web theo yêu cầu</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link mypage-footer__link--highlight">Thiết kế web chuyên nghiệp</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Thiết kế web theo chuẩn Mobile</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="mypage-footer__support-section">
+          <div class="mypage-footer__support-group">
+            <h3 class="mypage-footer__heading">Hỗ trợ</h3>
+            <ul class="mypage-footer__list">
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Câu hỏi thường gặp</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Quy trình thiết kế web</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Điều kiện giao dịch chung</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Bàn giao và phương thức thanh toán</a>
+              </li>
+              <li class="mypage-footer__list-item">
+                <a href="#" class="mypage-footer__link">Bảo mật thông tin</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="mypage-footer__bottom-border">
+        <div class="mypage-footer__bottom-content">
+          <div class="mypage-footer__copyright-section">
+            <p class="mypage-footer__copyright-text">
+              © Copyright 2025. Designed by MyPage - Bản quyền
+              <a href="#" class="mypage-footer__copyright-link">thiết kế web</a>
+              thuộc về MyPage.
+            </p>
+          </div>
+          <div class="mypage-footer__social-section">
+            <a href="#" class="mypage-footer__profile-link">HỒ SƠ NĂNG LỰC</a>
+            <div class="mypage-footer__social-icons">
+              <a href="#" class="mypage-footer__social-icon-wrapper">
+                <span class="mypage-footer__social-icon"></span>
+              </a>
+              <a href="#" class="mypage-footer__social-icon-wrapper">
+                <span class="mypage-footer__social-icon"></span>
+              </a>
+              <a href="#" class="mypage-footer__social-icon-wrapper">
+                <span class="mypage-footer__social-icon"></span>
+              </a>
+            </div>
+            <div class="mypage-footer__dmca-section">
+              <a href="#" class="mypage-footer__dmca-link">
+                <img src="/public/webest.png" alt="DMCA.com Protection Status" class="mypage-footer__dmca-image" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MyPageFooter',
+});
+</script>
+
+<style lang="scss" scoped>
+.mypage-footer {
+  background-color: #ecf0f4;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding-top: 39px;
+  padding-bottom: 24px;
+
+  &__container {
+    width: 1300px;
+    padding: 0 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+  }
+
+  &__divider {
+    position: absolute;
+    top: 0;
+    left: 15px;
+    width: 1270px;
+    height: 1px;
+    background-color: #999999;
+  }
+
+  &__content-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-end;
+    width: 100%;
+    margin-top: 0; /* Adjust as needed based on divider position */
+  }
+
+  &__info-section,
+  &__service-section,
+  &__design-section,
+  &__support-section {
+    padding: 0 15px;
+    flex: 1; /* Distribute space evenly */
+    min-width: 250px; /* Minimum width to prevent excessive squishing */
+  }
+
+  &__info-section {
+    width: 433.33px; /* Specific width from Figma */
+    height: 253.8px; /* Specific height from Figma */
+  }
+
+  &__info-group,
+  &__service-group,
+  &__design-group,
+  &__support-group {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    width: 100%;
+  }
+
+  &__heading {
+    font-family: 'Inter', sans-serif;
+    font-weight: 700;
+    font-size: 20.796875px;
+    line-height: 1.48;
+    text-transform: uppercase;
+    color: #008acd;
+    text-align: left;
+    margin-bottom: 0; /* Remove default margin */
+  }
+
+  &__list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 100%;
+  }
+
+  &__list-item {
+    display: flex;
+    align-items: center;
+    height: 26px; /* Fixed height from Figma */
+  }
+
+  &__icon-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 16px;
+    height: 16px;
+    margin-right: 3.84px; /* Adjusted from Figma layout */
+    color: #000000; /* Icon color */
+  }
+
+  &__text {
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+    font-size: 14.5px;
+    line-height: 1.793;
+    color: #000000;
+    text-align: left;
+    margin: 0;
+  }
+
+  &__separator {
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 1.625;
+    color: #000000;
+    margin: 0 4px; /* Adjusted for spacing */
+  }
+
+  &__symbol {
+    font-family: 'Font Awesome 5 Free', sans-serif;
+    font-weight: 900;
+    font-size: 16px;
+    line-height: 1;
+    color: #000000;
+    margin-right: 5px; /* Spacing for symbol */
+  }
+
+  &__link {
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+    font-size: 15.375px;
+    line-height: 1.691;
+    color: #000000;
+    text-decoration: none;
+    text-align: left;
+
+    &--highlight {
+      font-weight: 400; /* Figma shows 400, but textStyle_UTQG7L has 400 */
+      font-size: 14.375px; /* Figma shows 14.375, but textStyle_UTQG7L has 14.375 */
+      line-height: 1.808; /* Figma shows 1.808, but textStyle_UTQG7L has 1.808 */
+    }
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  &__bottom-border {
+    width: 100%;
+    height: 143px;
+    border-top: 1px solid #999999;
+    background-color: #333333;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 310px; /* Padding from Figma */
+  }
+
+  &__bottom-content {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 15px;
+  }
+
+  &__copyright-section {
+    padding: 10px 8px 10px 0;
+  }
+
+  &__copyright-text {
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+    font-size: 13.7109375px;
+    line-height: 1.896;
+    color: #ffffff;
+    margin: 0;
+  }
+
+  &__copyright-link {
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+    font-size: 13.59375px;
+    line-height: 1.912;
+    color: #ffffff;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  &__social-section {
+    display: flex;
+    align-items: center;
+    gap: 7.6px;
+    padding: 2px;
+  }
+
+  &__profile-link {
+    font-family: 'Inter', sans-serif;
+    font-weight: 700;
+    font-size: 14.296875px;
+    line-height: 1.818;
+    text-align: right;
+    color: #ffffff;
+    text-decoration: none;
+    margin-right: 20px; /* Adjusted spacing */
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  &__social-icons {
+    display: flex;
+    gap: 7.6px;
+  }
+
+  &__social-icon-wrapper {
+    width: 30px;
+    height: 30px;
+    border-radius: 30px;
+    background-color: transparent; /* Assuming no background from Figma data */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    color: #ffffff; /* Icon color */
+  }
+
+  &__social-icon {
+    font-family: 'Font Awesome 5 Brands', sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 1.642;
+    text-align: center;
+  }
+
+  &__dmca-section {
+    padding: 2.53px 0 0;
+  }
+
+  &__dmca-link {
+    display: block;
+  }
+
+  &__dmca-image {
+    width: 121px;
+    height: 24px;
+    display: block;
+  }
+}
+</style>
